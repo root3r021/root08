8debc4ef61b064e9c7964984c59e6a21a2c72254
Mehdi HS <m.h.1378.mh@gmail.com>
2016-04-09 22:28:39 +0430

    BlackPlus V 6.7

--- plugins/start.lua
+++ plugins/start.lua
@@ -3,7 +3,7 @@ do
 function run(msg, matches)
 local reply_id = msg['id']
 local text = 'Welcome to BlackPlus!\n\n>To get a new BlackPlus group, contact a support group:\n https://telegram.me/joinchat/A2ETvD160xvoj4y_pNAgig\n\nFor more information, check out our channels:\n\nChannel : @Black_CH\nSite : BlackPlus.ir \n\nUse #superhelp command to show bot commands!!\n\n#Thanks_for_using @BlackPlus!'
-if matches[1] == 'start' then
+if matches [1] == '/start' then
     if not is_sudo(msg) then
 reply_msg(reply_id, text, ok_cb, false)
 end
@@ -11,7 +11,7 @@ end
 end
 return {
 patterns = {
-    "[#!/]start"
+    "^/start"
 },
 run = run
 }
