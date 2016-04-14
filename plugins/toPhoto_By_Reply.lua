0d9785c2143c7538c584be8743a02a8df7929acf
Mehdi HS <m.h.1378.mh@gmail.com>
2016-04-15 01:35:26 +0430

    Fixed Bug

--- plugins/toPhoto_By_Reply.lua
+++ plugins/toPhoto_By_Reply.lua
@@ -1,7 +1,7 @@
 local function tosticker(msg, success, result)
   local receiver = get_receiver(msg)
   if success then
-    local file = '/root/blackplus/data/stickers/'..msg.from.id..'.webp'
+    local file = '/root/BlackPlus/data/stickers/'..msg.from.id..'.webp'
     print('File downloaded to:', result)
     os.rename(result, file)
     print('File moved to:', file)
@@ -29,7 +29,7 @@ end
 end
 return {
   patterns = {
-	"^[#!/](tosticker)$",
+	"^[#!/](sticker)$",
 	"%[(photo)%]",
   },
   run = run,
