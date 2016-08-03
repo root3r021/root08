aa1dd00e79fc5f51853c1aeb99699d51375651e3
Mehdi HS <m.h.1378.mh@gmail.com>
2016-08-03 21:49:31 +0430

    BlackPlus V 6.9

--- bot/bot.lua
+++ bot/bot.lua
@@ -212,297 +212,80 @@ function create_config( )
   -- A simple config with basic plugins and ourselves as privileged user
   config = {
     enabled_plugins = {
-    "plugins",
-    "antiSpam",
-    "antiArabic",
-    "banHammer",
-    "broadcast",
-    "inv",
-    "password",
-    "welcome",
-    "toSupport",
-    "me",
-    "toStciker_By_Reply",
-    "invSudo_Super",
-    "invSudo",
-    "cpu",
-    "badword",
-    "aparat",
-    "calculator",
-    "antiRejoin",
-    "pmLoad",
-    "inSudo",
-    "blackPlus",
-    "toSticker(Text_to_stick)",
-    "toPhoto_By_Reply",
-    "inPm",
-    "autoleave_Super",
-    "black",
-    "terminal",
-    "sudoers",
+    "Plugins",
+    "AboutMe",
+    "InSuper",
+    "InSudo",
+    "ToSupport",
+    "Welcome",
+    "SetWelcome",
+    "Inv",
+    "Weather",
+    "PmLoad",
+    "ServerMgr",
+    "Start",
+    "Start2",
+    "My_Msgs",
+    "TopStats",
+    "Whitelist",
+    "ToPhoto",
+    "ToSticker",
+    "OnService",
+    "Porn",
+    "Test",
+    "Time",
+    "Fantasy_Writer",
+    "Logo",
+    "MyNumber",
+    "PKGinastaller",
+    "BlackPlus",
+    "Black",
+    "ToVoice",
+    "AntiArabic",
+    "Downloader",
+    "Del_Pro",
+    "InAdmin",
+    "QrCode",
+    "Filter",
+    "Plist",
+    "Fwd_To_All",
+    "Fun",
+    "AntiLeave",
+    "AntiSpam",
+    "Aparat",
+    "BanHammer",
+    "Banner",
+    "Calculator",
+    "FeedBack",
+    "FileManager",
+    "InPm",
+    "InRealm",
+    "SpeedTest",
+    "StickerMaker",
+    "Instagram",
+    "Cpu",
+    "Webshot",
+    "ToPhoto_Txt_img",
+    "Gps",
+    "ToSticker(Text_to_stick)",
+    "Block",
+    "AntiPorn",
     "time",
-    "toPhoto",
-    "toPhoto_Txt_img",
-    "toSticker",
-    "toVoice",
-    "ver",
-    "start",
-    "whitelist",
-    "plist",
-    "inSuper",
-    "inRealm",
-    "onservice",
-    "inGroups",
-    "updater",
-    "qrCode",
-    "groupRequest_V2_Test",
-    "inAdmin"
-
+    "InGroups",
     },
-    sudo_users = {56693692},--Sudo users
+    sudo_users = {56693692,},--Sudo users
     moderation = {data = 'data/moderation.json'},
-    about_text = [[ ]],
-    help_text_realm = [[
-Realm Commands:
-#creategroup [Name]
-Create a group
-#createrealm [Name]
-Create a realm
-#setname [Name]
-Set realm name
-#setabout [group|sgroup] [GroupID] [Text]
-Set a group's about text
-#setrules [GroupID] [Text]
-Set a group's rules
-#lock [GroupID] [setting]
-Lock a group's setting
-#unlock [GroupID] [setting]
-Unock a group's setting
-#settings [group|sgroup] [GroupID]
-Set settings for GroupID
-#wholist
-Get a list of members in group/realm
-#who
-Get a file of members in group/realm
-#type
-Get group type
-#addadmin [id|username]
-Promote an admin by id OR username *Sudo only
-#removeadmin [id|username]
-Demote an admin by id OR username *Sudo only
-#list groups
-Get a list of all groups
-#list realms
-Get a list of all realms
-#support
-Promote user to support
-#-support
-Demote user from support
-#log
-Get a logfile of current group or realm
-#broadcast [text]
-#broadcast Hello !
-Send text to all groups
-Only sudo users can run this command
-#bc [group_id] [text]
-#bc 123456789 Hello !
-This command will send text to [group_id]
-**You can use "#", "!", or "/" to begin all commands
-*Only admins and sudo can add bots in group
-*Only admins and sudo can use kick,ban,unban,newlink,setphoto,setname,lock,unlock,set rules,set about and settings commands
-*Only admins and sudo can use res, setowner, commands
+    about_text = [[
 ]],
-    help_text = [[
-Commands list :
-#kick [username|id]
-You can also do it by reply
-#who
-Members list
-#modlist
-Moderators list
-#promote [username]
-Promote someone
-#demote [username]
-Demote someone
-#kickme
-Will kick user
-#about
-Group description
-#setname [name]
-Set group name
-#rules
-Group rules
-#id
-return group id or user id
-#help
-Returns help text
-#lock [links|flood|spam|Arabic|member|rtl|sticker|contacts|strict]
-Lock group settings
-*rtl: Kick user if Right To Left Char. is in name*
-#unlock [links|flood|spam|Arabic|member|rtl|sticker|contacts|strict]
-Unlock group settings
-*rtl: Kick user if Right To Left Char. is in name*
-#mute [all|audio|gifs|photo|video]
-mute group message types
-*If "muted" message type: user is kicked if message type is posted 
-#unmute [all|audio|gifs|photo|video]
-Unmute group message types
-*If "unmuted" message type: user is not kicked if message type is posted 
-#set rules <text>
-Set <text> as rules
-#set about <text>
-Set <text> as about
-#settings
-Returns group settings
-#muteslist
-Returns mutes for chat
-#muteuser [username]
-Mute a user in chat
-*user is kicked if they talk
-*only owners can mute | mods and owners can unmute
-#mutelist
-Returns list of muted users in chat
-#newlink
-create/revoke your group link
-#link
-returns group link
-#owner
-returns group owner id
-#setowner [id]
-Will set id as owner
-#setflood [value]
-Set [value] as flood sensitivity
-#stats
-Simple message statistics
-#save [value] <text>
-Save <text> as [value]
-#get [value]
-Returns text of [value]
-#clean [modlist|rules|about]
-Will clear [modlist|rules|about] and set it to nil
-#res [username]
-returns user id
-"!res @username"
-#log
-Returns group logs
-#banlist
-will return group ban list
-other commands :
-#vc [text]
-#tosticker
-#tophoto
-#webshot [url]
-#qr [text|link]
-#echo [text]
-#reqgp
-#insta [id|video/photo link]
-#tosupport
-#version
-**You can use "#", "!", or "/" to begin all commands
-*Only owner and mods can add bots in group
-*Only moderators and owner can use kick,ban,unban,newlink,link,setphoto,setname,lock,unlock,set rules,set about and settings commands
-*Only owner can use res,setowner,promote,demote and log commands
+    help_text_realm = [[
+
+]],
+    help_text = [[ 
+
 ]],
 	help_text_super =[[
-SuperGroup Commands:
-#info
-Displays general info about the SuperGroup
-#admins
-Returns SuperGroup admins list
-#owner
-Returns group owner
-#modlist
-Returns Moderators list
-#bots
-Lists bots in SuperGroup
-#who
-Lists all users in SuperGroup
-#kick
-Kicks a user from SuperGroup
-*Adds user to blocked list*
-#ban
-Bans user from the SuperGroup
-#unban
-Unbans user from the SuperGroup
-#id
-Return SuperGroup ID or user id
-*For userID's: !id @username or reply !id*
-#id from
-Get ID of user message is forwarded from
-#setowner
-Sets the SuperGroup owner
-#promote [username|id]
-Promote a SuperGroup moderator
-#demote [username|id]
-Demote a SuperGroup moderator
-#setname
-Sets the chat name
-#setrules
-Sets the chat rules
-#setabout
-Sets the about section in chat info(members list)
-#newlink
-Generates a new group link
-#link
-Retireives the group link
-#rules
-Retrieves the chat rules
-#lock [links|flood|spam|Arabic|member|rtl|sticker|contacts|strict|tgservice]
-Lock group settings
-*rtl: Delete msg if Right To Left Char. is in name*
-*strict: enable strict settings enforcement (violating user will be kicked)*
-#unlock [links|flood|spam|Arabic|member|rtl|sticker|contacts|strict|tgservice]
-Unlock group settings
-*rtl: Delete msg if Right To Left Char. is in name*
-*strict: disable strict settings enforcement (violating user will not be kicked)*
-#mute [all|audio|gifs|photo|video]
-mute group message types
-*A "muted" message type is auto-deleted if posted
-#unmute [all|audio|gifs|photo|video]
-Unmute group message types
-*A "unmuted" message type is not auto-deleted if posted
-#setflood [value]
-Set [value] as flood sensitivity
-#settings
-Returns chat settings
-#muteslist
-Returns mutes for chat
-#muteuser [username]
-Mute a user in chat
-*If a muted user posts a message, the message is deleted automaically
-*only owners can mute | mods and owners can unmute
-#mutelist
-Returns list of muted users in chat
-#banlist
-Returns SuperGroup ban list
-#clean [rules|about|modlist|mutelist]
-#del
-Deletes a message by reply
-#public [yes|no]
-Set chat visibility in pm !chats or !chatlist commands
-#res [username]
-Returns users name and id by username
-#log
-Returns group logs
-*Search for kick reasons using [#RTL|#spam|#lockmember]
-other commands :
-#vc [text]
-#tosticker
-#tophoto
-#webshot [url]
-#qr [text|link]
-#echo [text]
-#reqgp
-#insta [id|video/photo link]
-#tosupport
-#version
-#inv
-**You can use "#", "!", or "/" to begin all commands
-*Only owner can add members to SuperGroup
-(use invite link to invite)
-*Only moderators and owner can use block, ban, unban, newlink, link, setphoto, setname, lock, unlock, setrules, setabout and settings commands
-*Only owner can use res, setowner, promote, demote, and log commands
-Channel : @black_ch
+
+
 ]],
   }
   serialize_to_file(config, './data/config.lua')
