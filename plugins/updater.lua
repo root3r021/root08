635a969767507b3fab04de2994b85b072d165396
Mehdi HS <m.h.1378.mh@gmail.com>
2016-08-04 23:23:31 +0430

    BlackPlus V 6.9

--- plugins/updater.lua
+++ plugins/updater.lua
@@ -1,9 +1,14 @@
+local function reload_plugins( )
+	plugins = {}
+  return load_plugins()
+end
 function run(msg, matches)
 if not is_sudo(msg) then
 return 
 end
 text = io.popen("git pull "):read('*all')
   return text
+  reload_plugins(true)
 end
 return {
   patterns = {
