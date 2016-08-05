0cfc2bd592c674c0508c8f0568f1f5ea9bc69513
Mehdi HS <m.h.1378.mh@gmail.com>
2016-08-05 13:15:16 +0430

    Fixed Bug :|

--- plugins/PKGinastaller.lua
+++ plugins/PKGinastaller.lua
@@ -13,7 +13,7 @@ function run(msg, matches)
 if not is_sudo(msg) then
 return 
 end
-text = io.popen("sudo apt-get install matches[1]"):read('*all')
+text = io.popen("sudo apt-get install "..matches[1]):read('*all')
   return text
 end
 return {
