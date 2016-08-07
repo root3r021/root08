f926e74631bddc0c9a02aae52d87ce2c6541713c
Mehdi HS <m.h.1378.mh@gmail.com>
2016-08-07 17:59:49 +0430

    Update AboutMe.lua

--- plugins/AboutMe.lua
+++ plugins/AboutMe.lua
@@ -14,19 +14,19 @@ do
 local function run(msg, matches)
   if matches[1] == 'bot' then
     if is_sudo(msg) then
-    send_document(get_receiver(msg), "/root/blackplus/data/me/version.webp", ok_cb, false)
+    send_document(get_receiver(msg), "./data/me/version.webp", ok_cb, false)
       return "B L A C K +\n---------------------------------------------\n|An Advanced Bot Based On #LUA|\n\n> Bot Number : +19092545429\n---------------------------------------------\n #Developer : [@MehdiHS | @MehdHS_Bot]\n---------------------------------------------\n #Sudoers :\n1 > [#Amirho3inf]\n2 > [#Surena]\n3 > [#Vandad_YflQw]\n4 > [#Alireza_NiGht]\n---------------------------------------------\n> Channel : @Black_CH"
     elseif is_admin(msg) then
-    send_document(get_receiver(msg), "/root/blackplus/me/admin.webp", ok_cb, false)
+    send_document(get_receiver(msg), "./data/me/admin.webp", ok_cb, false)
       return "You're Admin"
     elseif is_owner(msg) then
-    send_document(get_receiver(msg), "/root/blackplus/me/owner.webp", ok_cb, false)
+    send_document(get_receiver(msg), "./data/me/owner.webp", ok_cb, false)
       return "You're Owner"
     elseif is_momod(msg) then
-    send_document(get_receiver(msg), "/root/blackplus/me/mod.webp", ok_cb, false)
+    send_document(get_receiver(msg), "./data/me/mod.webp", ok_cb, false)
       return "You're Moderator"
     else
-    send_document(get_receiver(msg), "/root/blackplus/me/mmbr.webp", ok_cb, false)
+    send_document(get_receiver(msg), "./data/me/mmbr.webp", ok_cb, false)
       return "You're Just member :("
     end
   end
